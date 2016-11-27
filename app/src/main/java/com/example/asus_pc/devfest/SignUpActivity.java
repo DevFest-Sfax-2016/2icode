ffe9d530848adc1e3ed6a7e57bb5b0332e0c59ba
ska007 <skandar.louati@enis.tn>
2016-11-27 08:08:48 +0100

    authentification v2

---
+++ app/src/main/java/com/example/asus_pc/devfest/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.example.asus_pc.devfest;
+        import android.app.Activity;
+import android.os.Bundle;
+
+public class SignUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_screen);
+    }
+
+
+
+}
